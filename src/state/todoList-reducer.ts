4015343456dc6d2386db21612983a490a58487dc
Alexander Kachan <akach.dev@mail.ru>
2023-09-19 10:03:16 +0300

    up lesson 8

--- src/state/todoList-reducer.ts
+++ src/state/todoList-reducer.ts
@@ -1,20 +1,25 @@
-import {TodolistType} from "../App";
-import {useId} from "react";
+import {FilterValuesType, TodolistType} from "../App";
 import {v1} from "uuid";
 
 type RemoveTodolistType = ReturnType<typeof removeTodolistAC>
 type AddTodoListType = ReturnType<typeof addTodoListAC>
+type ChangeTodoListTitleType = ReturnType<typeof changeTodoListTitleAC>
+type ChangeTodoListFilterType = ReturnType<typeof changeTodoListFilterAC>
 
-type ActionType = RemoveTodolistType | AddTodoListType
+type ActionType = RemoveTodolistType | AddTodoListType | ChangeTodoListTitleType | ChangeTodoListFilterType
 
-export const todoListReducer = (state: TodolistType[], action: ActionType) => {
+export const todoListReducer = (state: TodolistType[], action: ActionType): TodolistType[] => {
   switch (action.type) {
     case "REMOVE-TODOLIST":
       return state.filter(todo => todo.id !== action.id)
     case "ADD-TODOLIST":
       return [...state, {id: v1(), filter: "all", title: action.title}]
+    case "CHANGE-TODOLIST-TITLE":
+      return state.map(todo => todo.id === action.todolistId ? {...todo, title: action.title} : todo)
+    case "CHANGE-TODOLIST-FILTER":
+      return state.map(todo => todo.id === action.todolistId ? {...todo, filter: action.filter} : todo)
     default:
-      throw new Error("i don't understand this type")
+      return state
   }
 }
 
@@ -23,4 +28,10 @@ export const removeTodolistAC = (id: string) => {
 }
 export const addTodoListAC = (title: string) => {
   return {type: 'ADD-TODOLIST', title} as const
+}
+export const changeTodoListTitleAC = (todolistId: string, title: string) => {
+  return {type: 'CHANGE-TODOLIST-TITLE', title, todolistId} as const
+}
+export const changeTodoListFilterAC = (todolistId: string, filter: FilterValuesType) => {
+  return {type: 'CHANGE-TODOLIST-FILTER', todolistId, filter} as const
 }
